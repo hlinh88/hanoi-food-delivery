5103240829b8d3372990fb9a6e049cf444332c82
Hoang Linh <hlinh.jobs@gmail.com>
2022-06-30 22:47:12 +0700

    update project

--- src/components/recommendation/filter/FilterCategory.js
+++
@@ -1,8 +0,0 @@
-import { Text } from "react-native";
-import styles from "./styles";
-
-const FilterCategory = ({ category }) => {
-  return <Text style={styles.filterOption}>{category}</Text>;
-};
-
-export default FilterCategory;
